8dd4bc1cf14e033ca9437997090c8377be8788ca
Ubuntu <ubuntu@ip-172-31-36-252.ap-northeast-1.compute.internal>
2025-07-17 08:16:19 +0000

    3章まで

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,7 +3,14 @@ class ListsController < ApplicationController
     @list =List.new
   end
 
+  def create
+    list = List.new(list_params)
+    list.save
+    redirect_to '/top'
+  end
+
   def index
+    @lists = List.all
   end
 
   def show
@@ -11,4 +18,11 @@ class ListsController < ApplicationController
 
   def edit
   end
+
+  private
+  # ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body)
+  end
+
 end
